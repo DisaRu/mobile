03d9d7d1e80a6e72c74bf3e43a19f1c9c2eb1568
DisaRu <shishkin414@gmail.com>
2023-01-20 15:45:34 +0300

    slider done

--- promosite/app/js/slider.js
+++ promosite/app/js/slider.js
@@ -1,15 +1,45 @@
 const slider = document.querySelector('.slider');
-const sliderPrev = document.querySelector('.slider__button-prev');
-const sliderNext = document.querySelector('.slider__button-next');
+const sliderItem = document.querySelectorAll('.slider__item');
+const btnPrev = document.querySelector('.slider__button-prev')
+const btnNext = document.querySelector('.slider__button-next')
+let width;
+let count = 0;
 
-let sliderItemWrapper = document.querySelector('.slider-wrapper');
 
-addEventListener('resize', function(){
-console.log(sliderItemWrapper.offsetWidth);
+function init() {
+    console.log(width);
+    width = document.querySelector('.slider-wrapper').offsetWidth;
+    // slider.style.width = width * sliderItem.length + 'px';
+    sliderItem.forEach(item => {
+        item.style.width = width + 'px';
+    })
+    roll()
+}
+init()
+
+addEventListener('resize', init)
+
+btnNext.addEventListener('click', function(){
+    count++;
+    if(count >= sliderItem.length){
+        count = 0;
+    }
+    document.querySelector('.slider__count-show').innerText = count+1;
+    roll()
+    console.log(count+1);
+
+})
+btnPrev.addEventListener('click', function(){
+    count--;
+    if(count < 0 ){
+        count = sliderItem.length -1;
+    }
+    document.querySelector('.slider__count-show').innerText = count+1;
+    roll()
+    console.log(count+1);
+
 })
 
-
-sliderPrev.addEventListener('click', function(){
-    slider.style.width = 200+'px'
-
-})
+function roll(){
+    slider.style.transform = 'translate(-'+ count * width + 'px)'
+}
